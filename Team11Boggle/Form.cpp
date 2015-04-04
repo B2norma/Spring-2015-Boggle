9e80d2f8c244bec1495b25cfb9f4a0d560d63a68
Bradley Norman <Bradley.Norman91@gmail.com>
2015-04-04 18:50:35 -0400

    Changed project to a Windows Form App

---
+++ Team11Boggle/Form.cpp
@@ -0,0 +1,13 @@
+#include "Form.h"
+
+using namespace Team11Boggle;
+
+[STAThreadAttribute]
+int main(array<System::String ^> ^args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	Application::Run(gcnew MyForm());
+	return 0;
+}
