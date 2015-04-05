5d6128103713540616a78ab3650bba7c0cd71347
Tim Bergquist <tbergqu1@my.westga.edu>
2015-04-05 19:04:30 -0400

    Add ValidateWords to tracking

---
+++ Team11Boggle/ValidateWords.cpp
@@ -0,0 +1,18 @@
+#include "ValidateWords.h"
+
+/**
+* Initializes default ValidateWords object.
+*
+* @precondition none
+* 
+*/
+ValidateWords::ValidateWords()
+{
+}
+
+/**
+* Destructor called when the lifetime of a ValidateWords object ends.
+*/
+ValidateWords::~ValidateWords()
+{
+}
---
+++ Team11Boggle/ValidateWords.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include<string>
+
+using namespace std;
+
+ref class ValidateWords
+{
+private:
+
+public:
+	ValidateWords();
+	virtual ~ValidateWords();
+	bool search(string word);
+};
+
